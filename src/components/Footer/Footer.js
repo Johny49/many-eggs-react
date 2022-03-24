4152b221396b850a75f577d566069cad49a510f1
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-03-24 18:08:40 -0500

    created footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={classes["footer--pin"]}>
+      <h2>{`\u00a9`} 2022 Geoffrey Johnson</h2>
+    </footer>
+  );
+};
+
+export default Footer;
