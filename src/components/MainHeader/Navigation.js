1f930e0ffae36731d9c1cc68d6e581ed2798c176
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-03-26 13:07:57 -0500

    added logouthandler and conditional links

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,13 +1,39 @@
-import { useContext } from "react";
-// import authcontext from store
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { auth } from "../../config/firebase-config";
+import { logout, selectUser } from "../../store/user-slice";
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
-  // const authCtx = useContext(AuthContext);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  console.log(window.location.href); // TODO work with this
+
+  const logoutHandler = () => {
+    // dispatch store logout action
+    dispatch(logout());
+    // Firebase sign out
+    auth.signOut();
+  };
 
   return (
     <nav className={classes.nav}>
-      <button type="button">Sign Up</button>
+      {!user && (
+        <Link to="/auth">
+          <button type="button">Sign Up</button>
+        </Link>
+      )}
+      {user && (
+        <Link to="/profile">
+          <button type="button">Profile</button>
+        </Link>
+      )}
+      {user && (
+        <button type="button" onClick={logoutHandler}>
+          Logout
+        </button>
+      )}
     </nav>
   );
 };
