993e45def3114f1f33d618c502d09cb773ce4681
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-03-28 09:31:09 -0500

    added slice for grocery items

---
+++ src/store/grocery-slice.js
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const grocerySlice = createSlice({
+  name: "groceryItems",
+  initialState: {
+    items: [],
+    changed: false,
+  },
+  reducers: {
+    replaceGroceryList(state, action) {
+      state.items = action.payload.groceryItems;
+      state.changed = false;
+    },
+    addItemToList: (state, action) => {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.changed = true;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          quantity: newItem.quantity,
+          title: newItem.title,
+          isPurchased: newItem.isPurchased,
+        });
+      } else {
+        existingItem.quantity = existingItem.quantity + newItem.quantity;
+      }
+    },
+    RemoveItemFromList: (state, action) => {
+      const itemToRemove = action.payload;
+      const existingItem = state.items.find(
+        (item) => item.id === itemToRemove.id
+      );
+      state.changed = true;
+      if (existingItem.quantity - itemToRemove.quantity <= 0) {
+        state.items = state.items.filter((item) => item.id !== itemToRemove.id);
+      } else {
+        existingItem.quantity = existingItem.quantity - itemToRemove.quantity;
+      }
+    },
+  },
+});
+
+// actions
+export const groceryActions = grocerySlice.actions;
+// selector
+export const selectGroceryItems = (state) => state.groceryItems.items;
+export const selectGroceriesChanged = (state) => state.groceryItems.changed;
+// reducer
+export default grocerySlice.reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../store/user-slice";
+import groceryReducer from "../store/grocery-slice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    groceryItems: groceryReducer,
   },
 });
 
