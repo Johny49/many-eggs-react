f77aeb2c439053759c1df2174819b870441dedcd
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-03-28 09:38:00 -0500

    added read-write to firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { auth, onAuthStateChanged } from "./config/firebase-config";
-import { login, logout, selectUser } from "./store/user-slice";
+import {
+  auth,
+  onAuthStateChanged,
+  db,
+  set,
+  ref,
+  onValue,
+} from "./config/firebase-config";
+import { login, logout } from "./store/user-slice";
+import {
+  groceryActions,
+  selectGroceriesChanged,
+  selectGroceryItems,
+} from "./store/grocery-slice";
 // components and pages
 import MainHeader from "./components/MainHeader/MainHeader";
 import HomePage from "./pages/HomePage";
@@ -12,11 +24,24 @@ import Footer from "./components/Footer/Footer";
 
 import classes from "./App.module.css";
 
+// track inital load of app
+let isInitial = true;
+
 function App() {
+  const groceryItems = useSelector(selectGroceryItems);
+  const groceriesChanged = useSelector(selectGroceriesChanged);
   const dispatch = useDispatch();
 
-  // check if user is already authenticated
   useEffect(() => {
+    const fetchGroceries = (userId) => {
+      const groceryListRef = ref(db, `lists/${userId}-list`);
+      onValue(groceryListRef, (snapshot) => {
+        const groceryData = snapshot.val();
+        dispatch(groceryActions.replaceGroceryList(groceryData));
+      });
+    };
+
+    // check if user is already authenticated
     onAuthStateChanged(auth, (userAuth) => {
       if (userAuth) {
         // user logged in, store current user in state
@@ -27,11 +52,33 @@ function App() {
             displayName: userAuth.displayName,
           })
         );
+        //retrieve list for authenticated user
+        fetchGroceries(userAuth.uid);
       } else {
         dispatch(logout());
       }
     });
   }, [dispatch]);
+
+  useEffect(() => {
+    // first load of app
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    // update list in db when changed; skip on initial load
+    if (groceriesChanged) {
+      set(ref(db, "lists/" + auth.currentUser.uid + "-list"), {
+        groceryItems,
+      })
+        .then(() => {
+          console.log("data saved successfully!");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [groceryItems, groceriesChanged]);
 
   return (
     <div className="App">
