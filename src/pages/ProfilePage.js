51afef73d98b84719d64cfc491497e5e354bfeb4
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-03-29 12:13:59 -0500

    fixed access when logged out

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,4 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUser } from "../store/user-slice";
+
 const ProfilePage = () => {
+  const user = useSelector(selectUser);
+
+  if (!user) {
+    return <Navigate replace to="/" />;
+  }
+
   return <h2>This is the profile page ... </h2>;
 };
 
