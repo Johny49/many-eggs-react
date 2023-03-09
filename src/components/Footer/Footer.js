e32dae36ab219bb8379a57c3e67ccfe32eac200a
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2023-03-09 15:06:34 -0600

    update footer with image and dynamic year

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,17 @@
+import logoImg from "../../assets/header-logo.png";
 import classes from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={classes["footer--pin"]}>
-      <h2>{`\u00a9`} 2022 Geoffrey Johnson</h2>
+      <img
+        className={classes["footer-image"]}
+        src={logoImg}
+        alt={"A shopping cart filled with eggs"}
+      />
+      <h2>
+        {`\u00a9`} {new Date().getFullYear()} Geoffrey Johnson
+      </h2>
     </footer>
   );
 };
